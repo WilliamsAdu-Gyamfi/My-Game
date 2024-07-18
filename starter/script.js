329e42614171b7b266a11e7cda303f8233c3c7a2
williamsadu-gyamfi <adugyamfiwilliams23@gmail.com>
2024-07-18 15:56:03 -0700

    starting conditions

---
+++ starter/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const score1 = document.querySelector('#score--0');
+const score2 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+score1.textContent = 0;
+score2.textContent = 0;
+dice.classList.add('hidden');
